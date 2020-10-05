c03388bf31bbae40f2f930c306a933829f4a49b3
Dimoli <olifdmni@gmail.com>
2020-10-05 21:02:55 +0300

    Desktop draft

--- src/App.js
+++ src/App.js
@@ -1,3 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default () => <div className="App" />;
+import { menuItems, submenuItems, content } from "./assets/";
+
+export default () => {
+  const [activeMenuItem, setActiveMenuItem] = useState();
+
+  return (
+    <>
+      <nav className="navbar fixed-top justify-content-around">
+        {menuItems.map((item, index) => (
+          <a
+            key={index}
+            id={index}
+            className={`text-decoration-none text-dark ${
+              +activeMenuItem === index
+                ? "border-bottom border-primary"
+                : "border-none"
+            }`}
+            href="#!"
+            onMouseOver={(e) => setActiveMenuItem(e.target.id)}
+          >
+            {item}
+          </a>
+        ))}
+      </nav>
+      <div
+        className={`${
+          activeMenuItem ? "d-flex" : "d-none"
+        } justify-content-around position-absolute p-1 border border-primary border-top-0`}
+        style={{ width: "90%", overflowWrap: "anywhere", left: "5%" }}
+      >
+        {submenuItems[activeMenuItem]?.map((items, itemsIndex) => (
+          <div key={itemsIndex}>
+            {items.map((item, index) =>
+              index ? <h6 key={index}>{item}</h6> : <h5 key={index}>{item}</h5>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="mt-5" onMouseOver={(e) => setActiveMenuItem(undefined)}>
+        <p className="jumbotron">{content}</p>
+        {/* <i className="fa fa-bars" aria-hidden="true" /> */}
+      </div>
+    </>
+  );
+};
---
+++ src/assets/index.js
@@ -0,0 +1,81 @@
+export const menuItems = [
+  "Элемент 1",
+  "Элемент 2",
+  "Элемент 3",
+  "Элемент 4",
+  "Элемент 5",
+  "Элемент 6",
+];
+export const submenuItems = [
+  [
+    ["Элемент 1.1"],
+    ["Элемент 1.2"],
+    ["Элемент 1.3"],
+    ["Элемент 1.4"],
+    ["Элемент 1.5"],
+    ["Элемент 1.6"],
+  ],
+  [
+    ["Элемент 2.1"],
+    ["Элемент 2.2", "Элемент 2.2.1", "Элемент 2.2.2", "Элемент 2.2.3"],
+    ["Элемент 2.3"],
+    [
+      "Элемент 2.4",
+      "Элемент 2.4.1",
+      "Элемент 2.4.2",
+      "Элемент 2.4.3",
+      "Элемент 2.4.3",
+      "Элемент 2.4.5",
+      "Элемент 2.4.6",
+      "Элемент 2.4.7",
+    ],
+    ["Элемент 2.5"],
+    ["Элемент 2.6"],
+  ],
+  [
+    [
+      "Элемент 3.1",
+      "Элемент 3.1.1",
+      "Элемент 3.1.2",
+      "Элемент 3.1.3",
+      "Элемент 3.1.4",
+    ],
+    ["Элемент 3.2"],
+    ["Элемент 3.3"],
+  ],
+  [["Элемент 4.1"], ["Элемент 4.2"]],
+  [
+    [
+      "Элемент 5.1",
+      "Элемент 5.1.1",
+      "Элемент 5.1.2",
+      "Элемент 5.1.3",
+      "Элемент 5.1.4",
+    ],
+    ["Элемент 5.2", "Элемент 5.2.1"],
+    ["Элемент 5.3"],
+  ],
+  [
+    ["Элемент 6.1"],
+    ["Элемент 6.2", "Элемент 6.2.1", "Элемент 6.2.2"],
+    ["Элемент 6.3", "Элемент 6.3.1"],
+    ["Элемент 6.4"],
+  ],
+];
+export const content = `Consectetur pariatur esse tempor nostrud nulla veniam deserunt incididunt reprehenderit laborum. Cupidatat magna irure dolor magna in velit ex cupidatat commodo esse enim ullamco aute. Deserunt duis amet nostrud pariatur deserunt ut elit excepteur aliqua nisi anim ullamco exercitation. Aliqua ea adipisicing elit commodo deserunt.
+
+Nulla cupidatat nulla veniam ipsum nisi ullamco eiusmod enim labore. Labore voluptate qui enim Lorem et fugiat amet ex velit incididunt. Amet officia veniam esse quis do culpa cupidatat incididunt nulla incididunt irure Lorem. Eu do sint occaecat est.
+
+Deserunt ad esse amet consequat duis nisi reprehenderit deserunt excepteur irure consequat dolor fugiat reprehenderit. Proident pariatur eu et dolor Lorem. Amet fugiat sunt nostrud consequat. Ullamco officia culpa elit anim nisi consectetur ex nisi dolor enim.
+
+Ea mollit culpa sit magna veniam mollit esse in non ex aute adipisicing eu laborum. Ut aliquip ex quis id mollit sit occaecat Lorem consequat. Proident officia Lorem aute aute.
+
+Velit do veniam do commodo non consectetur sunt ipsum non. Elit est velit dolore deserunt laboris irure enim. Id fugiat eu ipsum culpa pariatur labore mollit do irure exercitation officia. Nulla cupidatat mollit sunt ut sunt sunt exercitation culpa sit ad ad. Laboris nostrud id ipsum anim esse elit.
+
+Laborum mollit pariatur in eu minim proident occaecat ex occaecat cupidatat elit eu. Consectetur anim anim eiusmod aliquip culpa esse excepteur sunt aliqua qui veniam Lorem consequat. Exercitation officia elit velit labore incididunt magna magna Lorem ex elit magna minim voluptate cillum. In ut commodo fugiat pariatur nulla in tempor magna consectetur magna. Deserunt occaecat enim proident excepteur id pariatur culpa ipsum duis eu qui tempor est duis. Ad duis dolor laboris tempor velit culpa.
+
+Do velit reprehenderit commodo reprehenderit aliqua esse consectetur excepteur dolor minim magna ipsum Lorem fugiat. Ullamco esse ex aliquip incididunt eiusmod sit mollit veniam elit elit quis cillum. Ullamco eiusmod consectetur ullamco velit pariatur sit. Do incididunt minim in ut est do aliqua anim nostrud culpa ipsum nulla. Dolore et officia ex amet.
+
+Fugiat pariatur enim qui eiusmod excepteur aliquip sit qui. Cillum tempor eu ad cupidatat amet. Eu et voluptate cillum occaecat proident duis sit duis.
+
+Ipsum ullamco dolor aliquip ipsum fugiat eiusmod voluptate sunt sit nostrud. Aliquip tempor consequat adipisicing enim adipisicing elit culpa reprehenderit labore ullamco. Consequat exercitation velit non pariatur do enim laborum culpa dolore eiusmod. Elit irure exercitation consectetur duis. Dolor Lorem laboris aliqua dolor nisi.`;
